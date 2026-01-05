3d8b040eb0127c413ee45176ca8684cb216e6f4f
Software Engineer-Data Engineer <118492176+divithraju@users.noreply.github.com>
2026-01-05 11:03:18 +0530

    Create crud.py

---
+++ backend/app/crud.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Session
+from .models import Product
+from .schemas import ProductCreate
+
+def create_product(db: Session, product: ProductCreate):
+    db_product = Product(**product.dict())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+
+def get_products(db: Session):
+    return db.query(Product).all()
